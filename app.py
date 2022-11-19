80a6c528883e89cc69731665d1f6e808207e6c15
adityaseth8 <aseth@ucdavis.edu>
2022-11-19 01:05:41 +0000

    Added rest of Luke's graphs

--- app.py
+++ app.py
@@ -40,9 +40,34 @@ introText='''Running a business often boils down to two main goals: Maximize pro
             '''
 tools='Python (pandas, matplotlib, scikit-learn), Anaconda, Jupyter Notebook, Git, Github, Scrum'
 
-#barplot of customer age
+#barplot of order prices over time
 age_df = px.data.tips()
 fig = px.histogram(
+    orders, 
+    x = "delivery_date", 
+    y='payment',
+    hover_data = orders.columns #interactive element
+)
+
+fig.update_layout(#reconfigure labels
+        title = {
+            'text' : 'Monthly Order Payment Distribution - 2021',
+            'y':0.95,
+            'x':0.45,
+            'xanchor':'center',
+            'yanchor':'top'
+        },
+        xaxis_title='Month of Payment',
+        yaxis_title='Payment Total(AUD)',
+        font=dict(
+            family='monospace', 
+            size=15, 
+            color='Black'))
+
+
+#barplot of customer age
+age_df = px.data.tips()
+fig2 = px.histogram(
     customers, 
     x = "age",  
     color = 'gender', 
@@ -50,7 +75,7 @@ fig = px.histogram(
     hover_data = customers.columns #interactive element
 )
 
-fig.update_layout(#reconfigure labels
+fig2.update_layout(#reconfigure labels
     title = {
         'text' : 'Distribution of Customer Age Grouped by Gender',
         'y':0.95,
@@ -62,6 +87,77 @@ fig.update_layout(#reconfigure labels
     yaxis_title='Total Number of Customers',
     legend_title= 'Gender',
     font=dict(family='monospace', size=15,  color='Black'))
+
+#barplot of order prices over time
+age_df = px.data.tips()
+fig3 = px.histogram(
+    orders, 
+    x = "delivery_date", 
+    y='payment',
+    hover_data = orders.columns #interactive element
+)
+
+fig3.update_layout(#reconfigure labels
+        title = {
+            'text' : 'Monthly Order Payment Distribution - 2021',
+            'y':0.95,
+            'x':0.45,
+            'xanchor':'center',
+            'yanchor':'top'
+        },
+        xaxis_title='Month of Payment',
+        yaxis_title='Payment Total(AUD)',
+        font=dict(
+            family='monospace', 
+            size=15, 
+            color='Black'))
+
+#histogram of product type by quantity
+age_df = px.data.tips()
+fig4 = px.histogram(
+    products, 
+    x = 'product_type',  
+    y = 'quantity',
+    color = 'product_name', 
+    hover_data = products.columns #interactive element
+)
+
+fig4.update_layout(#reconfigure labels
+    title = {
+        'text' : 'Total Number of Products',
+        'y':0.95,
+        'x':0.45,
+        'xanchor':'center',
+        'yanchor':'top'
+    },
+    xaxis_title='Product Type',
+    yaxis_title='Total Number of Product',
+    legend_title= 'Size',
+    font=dict(family='monospace', size=15,  color='Black'))
+
+#histogram of product type by price
+age_df = px.data.tips()
+fig5 = px.histogram(
+    products, 
+    x = 'product_type',  
+    y = 'price',
+    color = 'product_name', 
+    hover_data = products.columns #interactive element
+)
+
+fig5.update_layout(#reconfigure labels
+    title = {
+        'text' : 'Total Price of Products', #make this title cleaner
+        'y':0.95,
+        'x':0.45,
+        'xanchor':'center',
+        'yanchor':'top'
+    },
+    xaxis_title='Product Type',
+    yaxis_title='Product Price(AUD)',
+    legend_title= 'Size',
+    font=dict(family='monospace', size=15,  color='Black'))
+
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -80,15 +176,30 @@ app.layout = html.Div(children=[
     html.H3('Tools'),
     html.P(tools),
     html.Div('Team'),
-    html.H1('TESTING FOR AUTOMATIC DEPLOYMENT'),
     html.Div('Methodology'),
     # dcc.Graph(
     #     id='flyingdog',
     #     figure=beer_fig
     # ),
     dcc.Graph(
-        id="graph1",
+        id='graph1',
         figure=fig
+    ),
+    dcc.Graph(
+        id="graph2",
+        figure=fig2
+    ),
+    dcc.Graph(
+        id="graph3",
+        figure=fig3
+    ),
+    dcc.Graph(
+        id="graph4",
+        figure=fig4
+    ),
+    dcc.Graph(
+        id="graph5",
+        figure=fig5
     ),
     html.A('Code on Github', href=githublink),
     html.Br(),
